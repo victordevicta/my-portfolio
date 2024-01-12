1f7fa557f36fdbfdc2b335e24567ba07d729dfdc
Victor de Victa <victordevicta@gmail.com>
2024-01-12 14:46:21 +1100

    feat: navBar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+        <Link href={"/"} className="text-5xl text-white font-semibold">
+          LOGO
+        </Link>
+        <div className="menu"></div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import NavBar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <NavBar />
       <div className="container mx-auto px-12 py-4">
         <HeroSection />
       </div>
